90ea615a6d07bdbd90bdc5c166d464585630c863
Jeremy Dube <dub377@gmail.com>
2017-10-31 21:44:04 -0400

    Finished landing page

--- glozzom/src/js/main.js
+++ glozzom/src/js/main.js
@@ -10,3 +10,14 @@ $(function () {
 		});
 	});
 });
+
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
+
+// $('.slider').slick({
+// 	infinite: true,
+// 	slideToShow: 1,
+// 	slideToScroll: 1
+// });
